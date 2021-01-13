b059b741d7c415d2e6ca40e2150bde0d5b3aac5a
Srushti Desai <46123453+Srushti24@users.noreply.github.com>
2021-01-13 02:00:39 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const person_info = require('./controllers/person_info');
-
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
 
 var sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+app.use(express.json()); 
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
@@ -19,19 +19,21 @@ app.get("/", (req, res) => {
 
 
 
-app.get("/api", (req, res) => {
+app.get("/api/al", (req, res) => {
 
-
-
-
-    //person_info.getDistance(temp, 8, 10, temp.length);
-    var temp = [1, 2, 3];
-    person_info.get_pref(temp, 8, 10, temp.length);
-    // person_info.meet_criterias(temp, 8, 10, temp.length);
+    //person_info.check_if_present(temp,8,10);
 });
 
 
-    app.post("/api", (req, res) => {
-        console.log(req.body);
+    app.post("/api", function(req, res){
+        var temp = req.body.id;
+        var start = req.body.start;
+        //console.log(property)
+        
+        console.log(temp);
+        // person_info.check_if_present(temp, start, start+2);
+        person_info.check_if_present([1, 2, 3], 8, 10);
         res.json("hello world");
-    });
+});
+
+app.delete("/api")
