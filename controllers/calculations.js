ef7b2cbdc13a1f7da09cde799f24979a8a68a282
Srushti Desai <46123453+Srushti24@users.noreply.github.com>
2021-01-13 00:45:31 -0500

    Add files via upload

--- controllers/calculations.js
+++ controllers/calculations.js
@@ -1,7 +1,11 @@
+const person_info = require('./person_info');
 
-function calculator(preference, restaurant_id, temp, start, end, members) {
+//helps in filtering out only those retaurants that meets the  dietry criteria of the group
+function calculator(preference, restaurant_id, temp, start, end, x,  y) {
     var arr = [];
     var restaurant_criteria = [];
+    console.log(preference);
+    console.log(restaurant_id);
     for (var i = 0; i < preference.length; i++)
     {
         arr.push(preference[i] + "-" + "friendly");
@@ -14,14 +18,27 @@ function calculator(preference, restaurant_id, temp, start, end, members) {
             restaurant_criteria.push(restaurant_id[i]);
         }
     }
+    if (restaurant_criteria.length < 0) {
+        console.log("No such restaurant present which meets the groups requirements");
+    }
+    else if (restaurant_criteria.length == 1) {
+        console.log(restaurant_criteria);
+    }
+    else {
+        console.log("bcd");
+    }
+   
+    }
    
 
-}
 
 
 
 
 
+
+
+//Checks if the restaurant meets all the dietry need of the group
 function checkCriteria_two(arr,temp) {
     const result = arr.every(val => temp.includes(val));
     return result;
--- controllers/person_info.js
+++ controllers/person_info.js
@@ -9,149 +9,139 @@ var db = new sqlite3.Database('C:/Users/srush/OneDrive/Desktop/database/db.db', 
         console.log("no error");
     }
 });
+var counter = 0;
 
 
+//First gets preference of the entire group,
+//then the name of restaurants that meet the criteria of time, tables
+//Then get the average distance of all the members in the group
+// There is a helper function which is used, which thorws promise for my async function to handle
+//promises and async-await used as alot of database queires being made
 
-
-
-
-//The restaurant has that many tables at that particular time
-function checkCriteria_one(preference,temp,start,end,members)
-{
-    var s;
+async function parent(temp, start, end) {
+    var preference = [];
+    var firstmap = new Map();
     var restaurant_id = [];
-    if (members > 4 && members <= 6) {
-        s = "six";
+    var x=0;
+    var y=0;
+    
+    var s = temp.join(",");
+
+    //get preference of all people
+    let sql1 = `SELECT preference FROM Persons_details WHERE person_id IN ( ${s})`;
+    var row1 = await helper(sql1,temp, start, end,s);
+    for (var r in row1) {
+        if (!firstmap.has(row1[r].preference)) {
+            firstmap.set(row1[r].preference, r);
+            preference.push(row1[r].preference);
+        }
     }
-    else if (members > 2 && members <= 4) {
-        s = "four";
+
+    // get restaurants that meet criteria
+   
+    var a;
+    if (temp.length > 4 && temp.length <= 6) {
+        a = "six";
+    }
+    else if (temp.length > 2 && temp.length <= 4) {
+        a = "four";
     }
     else {
-        s = "two";
+        a = "two";
     }
-    let sql = `SELECT * FROM Restaurant WHERE start<=${start} AND ${end}<=end AND no_of_${s}>=1`;
-    console.log(sql);
-    var f = false;
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-        else {
-            rows.forEach((row) => {
-                console.log("final row name " + row.id);
-               
-                restaurant_id.push(row);
-                
-            });
+    let sql2 = `SELECT * FROM Restaurant WHERE start<=${start} AND ${end}<=end AND no_of_${a}>=1`;
+    var row2 = await helper(sql2, temp, start, end, a);
+    for (var r in row2) {
+        restaurant_id.push(row2[r]);
+     
+    }
 
-            calculations.calculator(preference, restaurant_id, temp, start, end, members);
-        }
 
-    });
-   
+    //get the average distance between friends
+    let sql3 = `SELECT location_x AS x, location_y AS y FROM Persons WHERE id IN ( ${s})`;
+    var row3 = await helper(sql3, temp, start, end, a);
+    for (var r in row3) {
+        x = x + row3[r].x;
+        y = y + row3[r].y;
+    }
+    x = x / temp.length;
+    y = y / temp.length;
+
+
+    calculations.calculator(preference, restaurant_id, temp, start, end, x, y);
 }
 
-// Each person's preference
-function get_pref(temp,start,end,members) {
-    var firstmap = new Map();
-    var preference = [];
-    var s = temp.join(",");
-   // console.log(s);
-    let sql = `SELECT preference FROM Persons_details WHERE person_id IN ( ${s})`;
-    const result = db.all(sql, [], (err, rows) => {
+
+
+function helper(sql,temp, start, end, s) {
+    return new Promise((resolve, reject) => {
+        db.all(sql, [], (err, row) => {
             if (err) {
-                console.log(err);
-                throw err;
+                reject(err);
             }
-            else {
-                // console.log("value is" + rows.length);
-                var i = 1;
-                rows.forEach((row) => {
-                    if (!firstmap.has(row.preference)) {
-                        firstmap.set(row.preference, i);
-                       preference.push(row.preference);
-                        i++;
-                        console.log("map size is" + firstmap.size);
-                    }
-                });
-                checkCriteria_one(preference, temp, start, end, members);
-              }
-    });
-
-}
-exports.get_pref = get_pref;
-
-//function getDistance()
-
-/*
-function getDistance(item, start, end, members) {
-    var x = 0;
-    var y = 0;
-    var s = item.join(",");
-    get_pref(item);
-    let sql = `SELECT location_x AS x, location_y AS y FROM Persons WHERE id IN ( ${s})`;
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-        else {
-            rows.forEach((row) => {
-                x = x + row.x;
-                y = y + row.y;
-            });
-            x = x / item.length;
-            y = y / item.length;
-            findRestaurant(x, y, start, end, members);
-        }
-    });
-}
-
-
-
-
-function findRestaurant(x, y, start, end, members) {
-    let firstmap = new Map();
-    var check = true;
-    var d = 5;
-    var prev_x = x;
-    var prev_y = y;
-    var counter = 0;
-    while (check == true && counter < 10) {
-        check = true;
-        let sql2 = `SELECT * FROM Restaurant WHERE location_x<=${x + d} AND location_x>=${prev_x} AND location_y<=${y + d} AND location_y>=${prev_y}`;
-        console.log(sql2);
-        db.all(sql2, [], (err, rows) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-            else {
-                rows.forEach((row) => {
-                    console.log("row id is" + row.id);
-                    var ans1 = checkCriteria_one(row.id, start, end, members);
-                    console.log("ans is" + ans1);
-                    check = !ans1;
-                    console.log("yolo");
-                });
-
-            };
+            resolve(row);
         });
-        prev_x = x + d;
-        prev_y = y + d;
-        d = d + 20;
-        counter++;
-    }
+    });
+}
+
+//First i check if any of those people have a reservation which conflicts with the new reservation
+//If thats the case no need for any calculations
+ function check_if_present(temp, start, end) {
+    var s = temp.join(",");
+    let sql = `SELECT * FROM Restaurant_relation WHERE start BETWEEN ${start - 2} AND ${start + 2} AND end BETWEEN ${end - 2} AND ${end + 2} AND person_id IN ( ${s})`;
+    console.log("sql is" + sql);
+     db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        else {
+            if (rows.length > 0) {
+                console.log("Not able to make reservation as a reservation is already present");
+            }
+            else {
+                parent(temp, start, end);
+            }
+
+        }
+
+
+    });
+
+}
+
+function book_seat(temp,restaurant_id,start,end) {
+    var placeholder = counter+","+restaurant_id + "," + start + "," + end;
+    let sql = `INSERT INTO Restaurant_relation(id,Restaurant_id,start,end) VALUES (` +placeholder `+)`;
+
+    // output the INSERT statement
+    console.log(sql);
+
+    db.run(sql, languages, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        else {
+
+            let sql = `INSERT INTO Restaurant_relation_details(relation_id,person_id) VALUES (` + counter`+)`;
+
+            // output the INSERT statement
+            console.log(sql);
+
+            db.run(sql, languages, function (err) {
+                if (err) {
+                    return console.error(err.message);
+                }
+                else {
+                }
+            });
+        }
+    });
+
 }
 
 
-*/
+exports.parent = parent;
+exports.check_if_present = check_if_present;
 
 
-
-
-
-//exports.getDistance = getDistance;
-
-//exports.meet_criterias = meet_criterias;
